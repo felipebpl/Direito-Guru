0be281e2daba43217d0f75b54288217ff3507fcb
Felipe <felipebpl@al.insper.edu.br>
2021-12-08 18:27:29 -0300

    P3 FINALIZADO -> funcionalida 2 feita: feed funcionando com post e get

--- frontend/direito-guru/src/App.js
+++ frontend/direito-guru/src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
+import FeedPage from './components/pages/Feed';
+import Cadastro from './components/pages/Cadastro';
 import { BrowserRouter as Router, Switch,   Route } from "react-router-dom";
 
  
@@ -16,6 +18,8 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/login' exact component={Login}/>
+        <Route path='/feed' exact component={FeedPage}/>
+        <Route path='/login' exact component={Cadastro}/>
       </Switch>
     </Router>
       </> 
